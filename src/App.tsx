3945f4b6f805b4f49e80bc77e72b5e158a7ed12b
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-01-22 14:27:32 -0700

    progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import React from 'react';
+import './App.css'
+
+type ProgressBarProps = {
+  percentage: number
+}
+export function ProgressBar({ percentage }: ProgressBarProps) {
+  const filled = `${percentage}%`
+  return <div className="progress">
+    <div className="bar" style={{
+      width: `${filled}`
+    }}>
+      <div className='percentage-container'>
+        <p className={
+          ['percentage',
+            percentage > 5 ? "" : "hidden"
+          ].join(" ")}>{filled}</p>
+      </div>
+    </div>
+  </div>;
+}
+
 export default function App() {
-  return (<div className="progress">
-    <h1>Hi</h1>
-  </div>);
+  const [filled, setFilled] = React.useState<number>(10);
+  return (
+    <div>
+      <h2>Progress So Far</h2>
+      <ProgressBar percentage={filled} />
+      <div className="progress-control">
+        <button
+          onClick={() => {
+            setFilled(Math.max(0, filled - 5));
+          }}
+          disabled={filled == 0}
+        >A little less</button>
+        <button
+          onClick={() => {
+            setFilled(Math.min(100, filled + 5));
+          }}
+          disabled={filled == 100}
+        >A little more</button>
+      </div>
+    </div>
+  )
 }
